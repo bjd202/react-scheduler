b0b33485786164f50b615241f47d8d106a930341
bjd202 <bjd202@naver.com>
2020-07-26 23:18:59 +0900

    200726_1

--- src/views/MainPage.js
+++ src/views/MainPage.js
@@ -1,22 +1,61 @@
-import React, { useEffect } from 'react'
-import {withRouter} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { withRouter } from 'react-router-dom'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from "@fullcalendar/interaction";
 import { Container } from 'reactstrap';
+import {
+    Button, Modal, ModalHeader, ModalBody, ModalFooter,
+    FormGroup, Input, Label, Card, CardBody
+} from 'reactstrap';
+import Datetime from 'react-datetime';
 
 function MainPage() {
+
+    const [toggleModalDemo, setToggleModalDemo] = useState(false);
+    const [startDate, setstartDate] = useState("")
+    const [endDate, setendDate] = useState("")
+    const [Events, setEvents] = useState([])
 
     useEffect(() => {
         document.body.classList.toggle("index-page");
     }, [])
 
-    const handleDateClick = (arg) => { 
-        alert(arg.dateStr)
-    }
+    const toggle = (arg) => {
+        setToggleModalDemo(!toggleModalDemo)
+        setstartDate(arg.dateStr);
+        setendDate(arg.dateStr);
+    };
 
     const eventClick = (arg) => {
-        console.dir(arg)
+        setToggleModalDemo(!toggleModalDemo)
+
+        setstartDate(arg.event.startStr);
+        setendDate(arg.event.endStr);
+        document.getElementById('title').value = arg.event.title
+        document.getElementById('content').value = arg.event.extendedProps.content
+        document.getElementById('desc').value = arg.event.extendedProps.desc
+
+    }
+    
+    const createEvent = () => {
+        const start_dt = document.getElementsByClassName('start_dt')[0].children[0].value
+        const end_dt = document.getElementsByClassName('end_dt')[0].children[0].value
+        const title = document.getElementById('title').value
+        const content = document.getElementById('content').value
+        const desc = document.getElementById('desc').value
+
+        setEvents([
+            ...Events,
+            {
+                title : title,
+                start : start_dt,
+                end : end_dt,
+                content : content,
+                desc : desc
+            }
+        ])
+
     }
 
     return (
@@ -31,18 +70,69 @@ function MainPage() {
                 <div className="squares square7" />
                 <Container>
                     <FullCalendar
-                        plugins={[ dayGridPlugin, interactionPlugin ]}
+                        plugins={[dayGridPlugin, interactionPlugin]}
                         initialView="dayGridMonth"
-                        dateClick={handleDateClick}
+                        dateClick={toggle}
                         eventClick={eventClick}
                         events={[
                             { title: 'event 1', date: '2020-07-21' },
-                            { title: 'event 2', date: '2020-07-22' }
+                            { title: 'event 2', date: '2020-07-22' },
+                            ...Events
                         ]}
-                        
                     />
+
+                    <Modal isOpen={toggleModalDemo} toggle={toggle}>
+                        <ModalBody>
+                            <Card>
+                                <CardBody>
+                                    <form>
+                                        <div className="form-row">
+                                            <FormGroup className="col-md-6">
+                                                <Label for="inputEmail4">시작일</Label>
+                                                <Datetime
+                                                    className="start_dt"
+                                                    value={startDate}
+                                                    timeFormat={false}
+                                                    dateFormat="YYYY-MM-DD"
+                                                    inputProps={{placeholder:"시작일"}}
+                                                    onChange={setstartDate}
+                                                />
+                                            </FormGroup>
+                                            <FormGroup className="col-md-6">
+                                                <Label for="inputPassword4">종료일</Label>
+                                                <Datetime
+                                                    className="end_dt"
+                                                    value={endDate}
+                                                    timeFormat={false}
+                                                    dateFormat="YYYY-MM-DD"
+                                                    inputProps={{placeholder:"종료일"}}
+                                                    onChange={setendDate}
+                                                />
+                                            </FormGroup>
+                                        </div>
+                                        <FormGroup>
+                                            <Label for="inputAddress">일정 제목</Label>
+                                            <Input type="text" id="title" name="title" />
+                                        </FormGroup>
+                                        <FormGroup>
+                                            <Label for="inputAddress2">일정 내용</Label>
+                                            <Input type="text" id="content" name="content" />
+                                        </FormGroup>
+                                        <FormGroup>
+                                            <Label for="inputAddress2">비고</Label>
+                                            <Input type="textarea" id="desc" name="desc" />
+                                        </FormGroup>
+                                        <Button type="button" color="primary" onClick={createEvent}>작성</Button>
+                                        <Button color="secondary" onClick={toggle}>
+                                            닫기
+                                        </Button>
+                                    </form>
+                                </CardBody>
+                            </Card>
+                        </ModalBody>
+                    </Modal>
                 </Container>
-                
+
             </div>
         </div>
     )
